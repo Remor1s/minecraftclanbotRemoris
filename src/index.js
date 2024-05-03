9b97474393de20ddf046c242eb1af8cb1687590b
Remor1s <168758957+Remor1s@users.noreply.github.com>
2024-05-04 00:46:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -151,9 +151,10 @@ async function startBot() {
 
 
 async function activateFunctions(bot) {
-    await scheduleMessage(bot, 'clan', 'реморис лох', 1)
+    await scheduleMessage(bot, 'clan', 'Вступай в наш клан за новостями о розыгрышах: https://t.me/ferndal', 1)
+    //  await scheduleMessage(bot, 'global', 'Вступай в наш клан ху', 1)
     //await scheduleMessage(bot, 'global', 'набор в клан', 15)
-    //await scheduleMessage(bot, 'local', 'реморис лох', 11)
+    //await scheduleMessage(bot, 'local', 'Вступай в наш тгк за новостями о розыгрышах: https://t.me/ferndal', 11)
 }
 
 startBot();
