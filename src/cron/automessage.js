e2500f921c339d86304835ae78873a227971fec1
Remor1s <168758957+Remor1s@users.noreply.github.com>
2024-05-04 11:46:32 +0300

    Update automessage.js

--- src/cron/automessage.js
+++ src/cron/automessage.js
@@ -3,7 +3,7 @@ const { scheduleMessage } = require("./auto");
 
 async function autoMessage(bot) {
     await scheduleMessage(bot, 'clan', 'Вступай в наш клан за новостями о розыгрышах: https://t.me/ferndal', 1)
-    await scheduleMessage(bot, 'global', '&f&lВсем привет! Вступй в наш клан. У нас каждую неделю проводятся розыгрыши на кейсы, где поучаствовать может каждый! Вступить в клан можно по команде /m ferndalebot join ', 1)
+    await scheduleMessage(bot, 'global', '&f&lВсем привет! Вступй в наш клан. У нас каждую неделю проводятся розыгрыши на кейсы, где поучаствовать может каждый! Вступить в клан можно по команде /m ferndalebot join ', 10)
     //await scheduleMessage(bot, 'global', 'набор в клан', 15)
     //await scheduleMessage(bot, 'local', 'Вступай в наш тгк за новостями о розыгрышах: https://t.me/ferndal', 11)
 }
